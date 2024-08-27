8f96f73fc353fdb28729b6072a1b9d44d4e4bcde
albiagro <alberto.agro@live.it>
2024-08-27 14:26:38 +0200

    Testing environmente variables

--- src/js/API_AllNews.js
+++ src/js/API_AllNews.js
@@ -1,5 +1,6 @@
 export default async() => {
     const response = await fetch(process.env.API_URL_ALL_NEWS);
+    console.log(process.env.API_URL_ALL_NEWS)
     const newsAll = await response.json();
     return newsAll;
   }
