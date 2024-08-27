536ae479e486220d2cb880fb9f868c9e80944c53
albiagro <alberto.agro@live.it>
2024-08-27 14:54:29 +0200

    Env. variables bug fixed

--- src/js/API_AllNews.js
+++ src/js/API_AllNews.js
@@ -1,6 +1,5 @@
 export default async() => {
     const response = await fetch(process.env.API_URL_ALL_NEWS);
-    console.log(process.env.API_URL_ALL_NEWS)
     const newsAll = await response.json();
     return newsAll;
   }
